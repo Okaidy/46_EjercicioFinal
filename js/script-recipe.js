e091665a62195e6de3fcc05155a8aecf52f368d6
Okaidy <okaidy.german@gmail.com>
2016-12-07 08:33:31 -0600

    Etapa 8

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -1,4 +1,18 @@
 $(document).ready( function() {
   console.log('El docuemnto de recipe esta listo  ;)');
 	$('.js-menu').hide();
+
+  showItems();
 });
+
+function showItems() {
+  $('.js-show-recipe').click(function () {
+    console.log('recipe');
+    $('.page.recipe').removeClass('make');
+  });
+
+  $('.js-show-make').click(function () {
+    console.log('make');
+    $('.page.recipe').addClass('make');
+  });
+}
